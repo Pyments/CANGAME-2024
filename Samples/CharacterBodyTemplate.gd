c077193488bb0df70e5da31315bf4aeeed4f8b5f
Eduardo-Pyment <edupymota@protonmail.com>
2024-05-26 17:41:17 -0300

    feat: fps headbob

--- Samples/CharacterBodyTemplate.gd
+++ Samples/CharacterBodyTemplate.gd
@@ -1,15 +1,19 @@
 extends CharacterBody3D
 
-
 const VELOCIDADE = 5.0
 const VELOCIDADE_PULO = 4.5
-const SENSIBILIDADE = 0.01
+const SENSIBILIDADE = 0.002
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Balanço da cabeça
+const BAL_FREQ = 2.0
+const BAL_QUANT = 0.08
+var t_bal = 0.0
+
+# Usa a gravidade das configurações do projeto 
 var gravidade = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var cabeca = $"Cabeça"
-@onready var camera = $"Cabeça/Camera3D"
+@onready var cabeca = $Cabeça
+@onready var camera = $Cabeça/Camera3D
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -32,12 +36,21 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (cabeca.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * VELOCIDADE
 		velocity.z = direction.z * VELOCIDADE
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0 
-
+	
+	# Balanço da cabeça
+	t_bal += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _balancoCabeca(t_bal)
+	
 	move_and_slide()
+	
+func _balancoCabeca(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BAL_FREQ) * BAL_QUANT
+	return pos
